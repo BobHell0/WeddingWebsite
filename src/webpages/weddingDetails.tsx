1c8a8276416f16d88b08d73d49d4a6532bb6801c
UNSW account <unswaccount@unknown.home>
2024-02-02 08:56:28 +1100

    wedding Details: added date + time section

--- src/webpages/weddingDetails.tsx
+++ src/webpages/weddingDetails.tsx
@@ -1,27 +1,62 @@
 import Navbar from "../components/Navbar";
-import weddingVenue from "../assets/images/weddingLocale.jpg";
-import "../components/CSS/weddingDetails.css";
+import "../components/CSS/weddingDetails1.css";
+import "../components/CSS/weddingDetails2.css";
+
+import weddingVenue from "../assets/images/weddingDetails/weddingLocale.jpg";
+import weddingCake from "../assets/images/weddingDetails/weddingCake.jpg";
+
 export default function weddingDetails() {
   const winWidth = window.outerWidth;
+  const imgWidth = (2 / 3) * winWidth;
+  const textSecWidth = winWidth - imgWidth;
+
   return (
     <>
       <Navbar />
-      <div id="container1">
-        <img
-          style={{ width: `${(2 / 3) * winWidth}px` }}
-          src={weddingVenue}
-        ></img>
-        <div style={{ width: `${(1 / 3) * winWidth}px` }} id="addressContainer">
-          <span className="addressText" style={{ fontSize: "42px" }}>
+      {/* Address Section */}
+      <div className="wdContainer">
+        <img style={{ width: `${imgWidth}px` }} src={weddingVenue}></img>
+        <div style={{ width: `${textSecWidth}px` }} className="textContainer">
+          <span
+            className="addressText"
+            style={{ fontSize: `${winWidth / 37}px` }}
+          >
             Address
           </span>
           <span
             className="addressText"
-            style={{ marginTop: "84px", fontSize: "28px" }}
+            style={{
+              marginTop: "8%",
+              fontSize: `${winWidth / 52}px`,
+            }}
           >
             Fake Venue
             <br></br>
             21 Fake Street, Fake Suburb 2100
+          </span>
+        </div>
+      </div>
+
+      {/* Date + Time Section */}
+      <div className="wdContainer">
+        <img
+          style={{ width: `${imgWidth}px`, float: "right" }}
+          src={weddingCake}
+        ></img>
+        <div style={{ width: `${textSecWidth}px` }} className="textContainer">
+          <span className="dateText" style={{ fontSize: `${winWidth / 37}px` }}>
+            Date & Time
+          </span>
+          <span
+            className="dateText"
+            style={{
+              marginTop: "8%",
+              fontSize: `${winWidth / 52}px`,
+            }}
+          >
+            10:00 am
+            <br></br>
+            1st of January, 2025
           </span>
         </div>
       </div>
