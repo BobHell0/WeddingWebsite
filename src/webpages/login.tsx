f014d295d4b04df2fc98c0dad239edb6754a4312
anujan-c <161555579+anujan-c@users.noreply.github.com>
2024-04-22 21:46:16 +1000

    wrote front end logic for handling inputted email and added types for events

--- src/webpages/login.tsx
+++ src/webpages/login.tsx
@@ -1,14 +1,37 @@
 import "../components/CSS/Login.css";
-import React, { useState } from "react";
+import React, { EventHandler, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [providedEmail, setProvidedEmail] = useState("");
+
+  const handleChangingEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event === null || event.target === null) {
+      return;
+    }
+    setProvidedEmail(event.target.value);
+    console.log(providedEmail);
+    return;
+  };
+
+  const handleLoginSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`./localhost:3000/checkLogin/${providedEmail}`, {
+      method: "GET",
+    });
+  };
+
   return (
     <>
       <div id="formContainer">
-        <form autoComplete="off" id="loginForm">
+        <form
+          autoComplete="off"
+          id="loginForm"
+          onSubmit={handleLoginSubmission}
+        >
           <label>
             <input
+              onChange={handleChangingEmail}
               type="text"
               className="loginInput"
               name="email"
