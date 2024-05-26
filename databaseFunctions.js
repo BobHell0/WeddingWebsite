33072a30e35849f08ffb5bfd39b041eb123a545d
anujan-c <161555579+anujan-c@users.noreply.github.com>
2024-05-26 11:04:01 +1000

    wrote database logic to write a csv file for the current guest list as reflected in mongodb

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -4,6 +4,8 @@ const uri = `mongodb+srv://bokchoyjunior:${mongoDB_password}@bobcluster.ky1jw4p.
 
 import GuestToId from './src/model/IndivGuest.js';
 import IdToGuests from './src/model/IdToGuests.js';
+import fs from 'fs'
+import { guestList_fileName } from './guestListFileName.js';
 
 
 export async function connectToMongo() {
@@ -33,5 +35,58 @@ export async function getFamily(groupId) {
   return {
     coming: lookup[0].coming,
     names: lookup[0].names
+  }
+}
+
+export async function setNewRsvpStatus(groupId, rsvpStatus) {
+  try {
+    const filter = { groupId: groupId }
+    const updateDoc = {
+      $set: {
+        coming: rsvpStatus
+      },
+    };
+
+    const result = await IdToGuests.updateOne(filter, updateDoc);
+    return result;
+  } catch (error) {
+
+    console.error(`Error thrown at databaseFunctions.js - setNewRsvpStatus`)
+    console.log(error.message)
+  }
+}
+
+export async function getTable() {
+  try {
+    const lookup = await IdToGuests.find()
+    // console.log(lookup)
+    const table = []
+    for (const family of lookup) {
+      for (const i in family.coming) {
+        console.log(`${family.names[i]} | ${family.coming[i]}`)
+        table.push({ name: family.names[i], rsvp: family.coming[i] });
+
+      }
+    }
+
+    const dataCSV = table.reduce((acc, guest) => {
+      acc += `${guest.name}, ${guest.rsvp}\n`;
+      return acc;
+    },
+      `name, rsvp\n` // column names for csv
+    );
+
+
+
+
+    fs.writeFileSync(`${guestList_fileName}`, `${dataCSV}\n`); // remove /n if you dont want new line at the end 
+
+
+
+    // finally, write csv content to a file using Node's fs module
+
+  } catch (error) {
+    console.error(`Error thrown at databaseFunctions.js - getTable`)
+    console.log(error.message)
   }
 }
